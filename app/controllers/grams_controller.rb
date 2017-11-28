68aab488c43878155cc224d3bd224c95c022ba91
terryllowery <terry@lowerytech.net>
2017-11-28 17:05:45 +0000

    Finished lesson 7 - setup form and controller for grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -5,4 +5,14 @@ class GramsController < ApplicationController
 	def new
 		@gram = Gram.new
 	end
+	def create
+		@gram = Gram.create(grams_params)
+		redirect_to root_path
+	end
+
+	private
+	def grams_params
+		params.require(:gram).permit(:message)
+	end
+
 end
