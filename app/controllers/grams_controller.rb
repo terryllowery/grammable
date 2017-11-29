74bf5079af67da50939008366b45fcf3281bbe7c
terryllowery <terry@lowerytech.net>
2017-11-29 01:33:31 +0000

    Added show call to retreive a single gram and verified tests passing

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,11 +1,19 @@
 class GramsController < ApplicationController
 	before_action :authenticate_user!, only: [:new, :create]
 	def index
-
 	end
+
+	def show
+		@grams = Gram.find_by_id(params[:id])
+		if @grams.blank?
+			render :new, status: :not_found
+		end
+	end
+
 	def new
 		@gram = Gram.new
 	end
+
 	def create
 		@gram = current_user.grams.create(grams_params)
 		if @gram.valid?
